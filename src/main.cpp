dc08112af6c0936340c299d21d91335f574d5174
Milan Tol <milantol35@gmail.com>
2025-12-27 22:42:01 +0100

    added init checkers

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 #include "events.hpp"
 #include "config.hpp"
-#include "graphics/draw_board.hpp"
 #include "checker.hpp"
+#include "graphics/draw_board.hpp"
+#include "graphics/draw_checkers.hpp"
+
+std::vector<Checker> init_position()
+{
+    std::vector<Checker> checkers;
+    //init black checkers
+    for (int i=0; i<20; i++)
+    {   
+        int square = 2*i;
+        int row = (2*i) % 10;
+        std::cout << row << std::endl;
+        if (!row%2)
+        {
+            square += 1; 
+        }
+        
+        checkers.push_back(Checker(square, 0));
+    }
+    //init white checkers
+    for (int i=0; i<20; i++)
+    {
+        checkers.push_back(Checker(99 - 2*i, 1));
+    }
+    return checkers;
+}
+
 
 int main()
 {
@@ -16,7 +42,10 @@ int main()
         
         window.clear();
 
+        std::vector<Checker> checkers = init_position();
+        
         drawBoard(window);
+        drawCheckers(window, checkers);
 
         window.display();
     }
