02f845f2903a1a5cb10eadab3d28d22ad218fe27
Milan Tol <milantol35@gmail.com>
2025-12-27 22:41:44 +0100

    .

--- src/config.hpp
+++ src/config.hpp
@@ -9,9 +9,9 @@ sf::Vector2u const window_size =  {1000, 1000};
 sf::Vector2f const window_size_f = {1000.0f, 1000.0f};
 
 //squares
- sf::Vector2f squareSize = 1.0f/10.0f * cfg::window_size_f;
+sf::Vector2f const squareSize = 1.0f/10.0f * cfg::window_size_f;
 
 //checkers configuration
-double const checker_radius = 60;
+double const checker_radius = 20;
 }
 
