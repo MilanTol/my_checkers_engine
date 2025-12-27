8caa860cbb7b09cf45e33db6ab7e7ee52b9678b4
Milan Tol <milantol35@gmail.com>
2025-12-27 20:13:48 +0100

    .

--- src/config.hpp
+++ src/config.hpp
@@ -5,7 +5,8 @@
 namespace cfg
 {
 //window configuration
-sf::Vector2f const window_size = {900.0f, 900.0f};
+sf::Vector2u const window_size =  {900, 900};
+sf::Vector2f const window_size_f = {900.0f, 900.0f};
 
 }
 
