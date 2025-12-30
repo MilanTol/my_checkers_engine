36bab7a4749861da691fc2a544f0cec53f59ab64
Milan Tol <milantol35@gmail.com>
2025-12-30 18:11:05 +0100

    added LegalMoves struct which contains all legal moves on the board

--- src/board/moves/legal_moves.hpp
+++ src/board/moves/legal_moves.hpp
@@ -1,14 +1,24 @@
-// #pragma once
+#pragma once
 
-// #include "move.hpp"
-// #include "legal_moves_from_square.hpp"
+#include "move.hpp"
+#include "legal_moves_from_square.hpp"
 
-// struct LegalMoves 
-// {
-//     std::vector<Move>
-//     LegalMoves(const Position& position)
-//     {
+struct LegalMoves 
+{
+    std::vector< std::vector<Move> > moves;
+    bool capturePossible = false;
 
-//     }
+    LegalMoves(const Position& position)
+    {
+        for (int i=0; i<100; i++)
+        {
+        LegalMovesFromSquare legal_moves_from_square = LegalMovesFromSquare(Square(i), position);
+            moves.push_back(legal_moves_from_square.moves);
+            if (legal_moves_from_square.capturePossible)
+            {
+                capturePossible = true;
+            }
+        }
+    }
 
-// };
+};
